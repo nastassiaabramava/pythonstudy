2f0ed1dff2daa76c81a6f6c4dd182ba966ae90f7
BYTE\a.abramava <a.abramava@nexters.com>
2025-08-06 16:51:44 +0300

    4.1.i

--- draft.py
+++ draft.py
@@ -1,8 +1,16 @@
-number = 12345
-def number_length(number):
-    number = str(abs(number))
-    length = len(number)
-    return length
+names = {
+    'ru': ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь',
+           'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+    'en': ['January', 'February', 'March', 'April', 'May', 'June',
+           'July', 'August', 'September', 'October', 'November', 'December']
+}
+
+def month(number, language):
+    return names[language][number - 1]
+result = month(1, "en")
+print(result)
+
+
 
 
 
