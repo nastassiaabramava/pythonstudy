6f978910cba3074ac756a8dca710e5ba9b41490e
BYTE\a.abramava <a.abramava@nexters.com>
2025-08-06 15:03:01 +0300

    test

--- draft.py
+++ draft.py
@@ -1,8 +1,22 @@
-number = 12345
-def number_length(number):
-    number = str(abs(number))
-    length = len(number)
-    return length
+def fragments(numbers):
+    if not numbers:
+        return []
+    spisok1 = [numbers[0]]
+    general_spisok = []
+
+    for i in range(1, len(numbers)):
+        if numbers[i] >= numbers[i - 1]:
+            spisok1.append(numbers[i])
+        else:
+            general_spisok.append(spisok1)
+            spisok1 = [numbers[i]]
+    general_spisok.append(spisok1)
+    return general_spisok
+
+result = fragments([])
+print(result)
+
+
 
 
 
