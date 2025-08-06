c1d776a99ddbcf17ece3248f5ef325f07469b9b8
BYTE\a.abramava <a.abramava@nexters.com>
2025-08-06 16:51:06 +0300

    4.1.i

---
+++ tasks-4.1/4.1.i.py
@@ -0,0 +1,10 @@
+names = {
+    'ru': ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь',
+           'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
+    'en': ['January', 'February', 'March', 'April', 'May', 'June',
+           'July', 'August', 'September', 'October', 'November', 'December']
+}
+
+
+def month(number, language):
+    return names[language][number - 1]
