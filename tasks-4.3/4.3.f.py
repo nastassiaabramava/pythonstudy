c9487724d7c18fb3f1f84dee23ca14acc1581042
BYTE\a.abramava <a.abramava@nexters.com>
2025-09-05 16:00:27 +0300

    4.3.f

--- tasks-4.3/4.3.f.py
+++ tasks-4.3/4.3.f.py
@@ -1,13 +1,22 @@
-def result_accumulator(func):
-    result = []
+def merge_sort(args=None):
+    if args is None:    # если список пуст, то это и есть ответ
+        return args
+    if len(args) == 1:    # если в списке 1 цифра, то это и есть ответ
+        return args
 
-    def wrapper(*args, **kwargs):
-        nonlocal result  # nonlocal видит на 1 уровень выше
-        value = func(*args)  # вызвали функцию из примера
-        result.append(value)  # результат добавили в список
-        if kwargs.get('method') == 'drop':
-            ans = result  # т.к. список будем очищать, то сделали его копию
-            result = []  # список очистили
-            return ans  # копию вернули
-
-    return wrapper
+    mid = len(args) // 2    # делим входной список на 2 части
+    left = merge_sort(args[:mid])    # левая часть от начала до середины
+    right = merge_sort(args[mid:])    # правая часть от середины до конца
+    result = []    # список для сортировки
+    i = j = 0    # переменные для сравнения
+    while i < len(left) and j < len(right):    # сравниваем и добавляем в список
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    # добавляем все, что осталось, если половинки были разными по количеству цифр
+    result.extend(left[i:])
+    result.extend(right[j:])
+    return result
