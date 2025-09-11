4eb89e11bd69e28505ce559fbaff39b7d2e493c1
nastassiaabramava <nastassia.abramava@gmail.com>
2025-09-11 18:54:44 +0300

    Update 4.2.o.py

--- tasks-4.2/4.2.o.py
+++ tasks-4.2/4.2.o.py
@@ -1,6 +1,7 @@
-def get_repeater(func, count):    # аргументы: некая функция (лямбда) и число (кол-во раз)
-    def new_func(x):    # создаем новую функцию, в котрой описываем func
-        for i in range(count):    # для каждого числа в диапазоне х-count
-            x = func(x)    # применяем функцию к х
-        return x    # new_func возвращает нам число
-    return new_func    # get_repeater возвращает функцию new_func
+def get_repeater(func, count):
+    def new_func(x, **kwargs):
+        y = x
+        for _ in range(count):
+            y = func(y, **kwargs)
+        return y
+    return new_func
