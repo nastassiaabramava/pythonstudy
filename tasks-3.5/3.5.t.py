85b8b5b81f31dd6bbac9d967be6fd2b5530d32d6
BYTE\a.abramava <a.abramava@nexters.com>
2025-11-13 00:20:03 +0200

    3.5.t

--- tasks-3.5/3.5.t.py
+++ tasks-3.5/3.5.t.py
@@ -1,8 +1,15 @@
-with open('numbers.num', encoding='utf-8') as file_in:
-    numbers = file_in.read()
-number_list = [num for line in numbers for num in line.split()]
-sum_number = 0
-for num in number_list:
-    number = int(num, 16)
-    sum_number += number
-print(sum_number % 65536)
+with open('numbers.num', 'rb') as f:
+    data = f.read()
+
+total = 0
+
+# идём по 2 байта: [hi][lo]
+for i in range(0, len(data), 2):
+    hi = data[i]
+    lo = data[i + 1]
+    value = hi * 256 + lo
+
+    # сумма в 2-байтном диапазоне
+    total = (total + value) % 65536
+
+print(total)
